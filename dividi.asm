6bd8769917916e47d7f005c30211a2360e7bc3b2
zumb08 <zumblack@protonmail.com>
2021-01-28 16:15:13 -0300

    up code

---
+++ dividi.asm
@@ -0,0 +1,29 @@
+.data 
+    x:  .asciiz "Digite o valor de X: " 
+    y:  .asciiz "Digite o valor de Y: "
+    z:  .asciiz "Digite o valor de Z: "
+
+
+.text 
+    li $v0, 4 # Receve  o número inteiro 4
+    la $a0, x # a0 = X
+    syscall # Executa   
+    li $v0, 5 # Recebe o numero inteiro 5
+    syscall
+    move $t1,$v0 # t1 = v0
+    li $v0, 4  # IMprimi uma string 
+    la $a0, y # a0 = y
+    syscall
+    li $v0, 5 # Recebe número inteiro 
+    syscall
+    move $t2,$v0 # t2 = v0
+    div $t3, $t1, $t2  # t3 = t2 / t1 
+    li $v0, 4 # Imprimi uma string 
+    la $a0, z  # a0 = z
+    syscall  # Execute 
+    li $v0, 1  # Imprime uma string 
+    la $a0,($t3)  # a0 = t3
+    syscall
+    li $v0, 10 # Prepara para ser finalizado 
+    syscall 
+
